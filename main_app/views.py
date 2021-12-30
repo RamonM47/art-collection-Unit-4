eca32fed67cda7e89eb08fd7dbb2ecbd5c96d2f6
RamonM47 <aviramon.mesa@gmail.com>
2021-12-30 14:09:02 -0500

    finish basic styling with hard coded elements with finished model structure

--- main_app/views.py
+++ main_app/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 class Post:
-  def __init__(self, name, goal, sketch, current_state):
+  def __init__(self, name, goal, sketch, current_state, published):
     self.name = name
     self.goal = goal
     self.sketch = sketch
     self.current_state = current_state
+    self.published = published
+
 
 posts = [
-  Post('Bold and Brash', 'to provide a vivid look into my life', 'https://preview.redd.it/ph1xtyeangy11.jpg?width=640&crop=smart&auto=webp&s=36bed1561760cb8a86b025c8ce49a4591b77f51d', 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/high-res-bold-and-brash-repaint-high-res-bold-and-brash-repaint.jpg'),
+  Post('Bold and Brash', 'to provide a vivid look into my life', 'https://preview.redd.it/ph1xtyeangy11.jpg?width=640&crop=smart&auto=webp&s=36bed1561760cb8a86b025c8ce49a4591b77f51d', 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/high-res-bold-and-brash-repaint-high-res-bold-and-brash-repaint.jpg', 'yes'),
 
-  Post('Gears of War sunset', 'to give a look into my xbox 360 breh', '', 'https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/bridge-to-eternity-michael-lang.jpg')
+  Post('Gears of War sunset', 'to give a look into my xbox 360 breh', '', 'https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/bridge-to-eternity-michael-lang.jpg', 'yes'),
+
+  Post('commision1', 'she wanted a drawing so i drew her a drawing', 'https://i.ibb.co/28SM6z9/2-v-Q3-PGo-A.jpg', '', 'no')
 
 ]
 

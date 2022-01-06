c2df61299d76f16bd7576270916847e54a1cdd16
RamonM47 <aviramon.mesa@gmail.com>
2022-01-06 11:23:02 -0500

    fix formatting inconsistencies in .html and .py files

--- main_app/models.py
+++ main_app/models.py
@@ -17,7 +17,11 @@ class Post(models.Model):
   sketch = models.URLField(max_length=500)
   current_state = models.URLField(max_length=500)
   updates = models.BooleanField(default='False')
-  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+  user = models.ForeignKey(
+    User, 
+    on_delete=models.CASCADE, 
+    null=True
+  )
 
   def worked_on_today(self):
     return self.work_set.filter(date=date.today()).count() >= len(TOD)
@@ -35,7 +39,10 @@ class Work(models.Model):
     choices=TOD, 
     default=TOD[0][0]
   )
-  progress_pic = URLField(max_length=250, default='https://toppng.com//public/uploads/preview/9d5-badly-drawn-thinking-emoji-11562863577r4jcmmnhf9.png')
+  progress_pic = URLField(
+    max_length=250, 
+    default='https://toppng.com//public/uploads/preview/9d5-badly-drawn-thinking-emoji-11562863577r4jcmmnhf9.png'
+  )
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
   def __str__(self):
@@ -46,7 +53,10 @@ class Work(models.Model):
 
 class Photo(models.Model):
   url = models.CharField(max_length=250)
-  post = models.OneToOneField(Post, on_delete=models.CASCADE)
+  post = models.OneToOneField(
+    Post, 
+    on_delete=models.CASCADE
+  )
 
   def __str__(self):
     return f"Photo for post_id: {self.post_id} @{self.url}."
